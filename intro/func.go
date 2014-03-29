75b801c8066d4271db3b6fd88b1d79da1a9def23
Rich Siegel <rsiegel@ise.com>
2014-03-29 14:48:30 -0400

    my first conditional - just like powershell

--- intro/func.go
+++ intro/func.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 
-func add(x,y int) (int, string) {
-    return x + y, "test"
+func add(x,y int) int {
+    return x + y
 }
 
 func main() {
-    fmt.Println(add(100, 200))
+    sum:=0
+    sum=add(100, 200)
+    if sum==300 {
+       fmt.Println("we added 100 and 200 correctly")
+    } else {
+       fmt.Println("computers suck at math")
+    }
 }
