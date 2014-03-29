512f50153486cedb125c7898ad971b8a99f527c3
Rich Siegel <rsiegel@ise.com>
2014-03-28 22:00:20 -0400

    some fun declarations

---
+++ intro/declares.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    // my birthday!
+    var i, j int = 9, 19
+    k := 75
+    booleana, kashafalsa, mystr := true, false, "no!"
+    fmt.Println("wassup")
+    fmt.Println(i, j, k, booleana, kashafalsa, mystr)
+}
